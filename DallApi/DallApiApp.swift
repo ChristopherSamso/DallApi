8b3bd6638bd6c303d64e186d8b95a64e7faf4dd5
ChristopherSamso <chsa6996@colorado.edu>
2022-12-08 14:20:17 -0700

    Initial Commit

---
+++ DallApi/DallApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DallApiApp.swift
+//  DallApi
+//
+//  Created by Christopher Samso on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DallApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
